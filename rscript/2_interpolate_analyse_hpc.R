483e409d6f8554e371cb3467d5d942613602cc2d
Barney Harris <barneyharris@CARCH051582.local>
2021-10-27 10:15:50 +0100

    added assign function for .MPIrun()

--- rscript/2_interpolate_analyse_hpc.R
+++ rscript/2_interpolate_analyse_hpc.R
@@ -1,3 +1,22 @@
+assign(".MPIrun", function() {
+  if (mpi.comm.rank(0) > 0){
+    sys.load.image(".RData",TRUE)
+    .First.sys()
+    sink(file="/dev/null")
+    slaveLoop(makeMPImaster())
+    mpi.quit()
+  }
+  else {
+    makeMPIcluster()
+    .Last <<- function(){
+      cl <- getMPIcluster()
+      if (! is.null(cl))
+        stopCluster(cl)
+      mpi.quit()
+    }
+  }
+}, envir = globalenv())
+
 library(Rmpi)
 library(snow)
 
