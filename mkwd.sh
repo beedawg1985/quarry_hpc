382de9147941294c5a27df6c8723bc4665f39504
Barney Harris <barneyharris@CARCH051582.local>
2021-12-09 10:33:20 +0000

    .

--- mkwd.sh
+++ mkwd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 # source /home/tcrnbgh/quarry_hpc/init.sh
 # remove and remake working dir
-rmdir ~/empty_dir
-mkdir ~/empty_dir
-rsync -a --delete ~/empty_dir/ ~/Scratch/quarry_data/
+rmdir /home/tcrnbgh/empty_dir
+mkdir /home/tcrnbgh/empty_dir
+rsync -a --delete /home/tcrnbgh/empty_dir/ /home/tcrnbgh/Scratch/quarry_data/
 mkdir /home/tcrnbgh/Scratch/quarry_data
 
 # make Renviron files
@@ -11,10 +11,8 @@ export TMPDIR=/home/tcrnbgh/Scratch/tmp
 echo "TMPDIR=/home/tcrnbgh/Scratch/tmp" > /home/tcrnbgh/Scratch/quarry_data/.Renviron
 cp /home/tcrnbgh/Scratch/quarry_data/.Renviron /home/tcrnbgh/.Renviron
 
-# run grass to initialise location - !! now done in R
+# make folder for grass locs
 mkdir /home/tcrnbgh/Scratch/quarry_data/grassdb
-# grass -c /home/tcrnbgh/quarry_hpc/vector/intout_1_training.gpkg /home/tcrnbgh/Scratch/quarry_data/grassdb/quarry -e
-
 export GISDBASE=/home/tcrnbgh/Scratch/quarry_data/grassdb
 
 # create output folder
