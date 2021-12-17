ce40a3402c835f12e450eac7a341e43545a5b1fe
Barney Harris <barneyharris@CARCH051582.local>
2021-12-17 15:24:51 +0000

    ml hole and smp routines

---
+++ rscript/2_interpolate_analyse_hpc_ml_hole2.R
@@ -0,0 +1,73 @@
+library(Rmpi) # callng this library will crash R unless you've submitted using qsub
+library(snow)
+
+setwd('/home/tcrnbgh/Scratch/quarry_data/quarry_hpc')
+sink('./2_interpolate_analyse_hpc_sinkout.txt')
+
+Sys.setenv(TMPDIR='/home/tcrnbgh/Scratch/tmp')
+print('main R tempdir is...')
+print(tempdir())
+
+# prepare data --------------------------------
+print('loading prepped data...')
+f <- 'data/prepData_nooffset_buffPol10_hole2_allsites.RDS'          # check this !
+prepData <- readRDS(f)
+length(prepData)
+
+print('done!')
+
+# interpolation run --------------------------------------------
+print('running interpolations...')
+st <- Sys.time()
+
+#!! if offSet = T the use pd$tiles$pol !!
+print('getting MPI cluster...')
+cl <- snow::getMPIcluster()
+print('done!')
+# Display info about each process in the cluster
+print(clusterCall(cl, function() Sys.info()))
+
+print('running interpolations...')
+# loop below runs in parallel
+datOut <- snow::clusterApply(cl, prepData, function(pd) {
+  
+  setwd('/home/tcrnbgh/Scratch/quarry_data/quarry_hpc')
+  source('rscript/interpolation_functions.R')
+  print('temp dir...')
+  print(tempdir())
+  Sys.setenv(TMPDIR='/home/tcrnbgh/Scratch/tmp')
+  
+  userDataDir <- '/home/tcrnbgh/Scratch/quarry_data'
+  grassGISDBASE <- paste0(userDataDir,'/grassdb')
+
+  sessionTag <- 'prepData_nooffset_buffPol10_hole2_allsites'        # check this !
+  
+  sink(paste0('logs/2_interpolate_analyse_hpc_sinkout_site',
+              pd$pol$fid,'.txt'))
+  
+  cvGrids <- loadCV()
+  
+  interpolateRas(pd,
+    paramData = cvGrids,
+    gLoc = grassGISDBASE,
+    outputDir = '/home/tcrnbgh/Scratch/quarry_data/data_output',
+    testCV = F, # = T for test run                                # check this !
+    outputTag = sessionTag,
+    intMethods=c(
+      'rfsp',
+      'nn','idw','ok','tin',
+      'gbicubic',
+      'gspline'
+      )
+    )
+  
+})
+save(datOut,
+     file=paste0('/home/tcrnbgh/Scratch/quarry_data/',
+                 'prepData_nooffset_buffPol10_hole2_allsites',       # check this !
+                 'int_times.RDS'))
+print('done!')
+# Clean up the cluster and release the relevant resources.
+stopCluster(cl)
+sink()
+mpi.quit()
